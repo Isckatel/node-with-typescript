14109b47324b54c54f231c522a55cda94d245ae0
i.egoshin <i.egoshin@prosoftsystems.ru>
2024-04-10 14:56:22 +0500

    step 3,4 homework

--- src/operations.test.ts
+++ src/operations.test.ts
@@ -1,12 +1,15 @@
 const assert = require("assert")
 const operations = require("./operations")
 
-it('the number should be returned', () => {
-    const a = 1, b = 2, c = 3
+//a*x^2 + b*x + c = 0, a != 0
+
+//тест, который проверяет, что для уравнения x^2+1 = 0 корней нет (возвращается пустой массив)
+it('x^2+1 = 0 корней нет', () => {
+    const a = 1, b = 0, c = 1
     const expected = 0
     const result = operations.equation.solve(a, b, c)
 
-    if(result!==expected){
+    if(result.length !== 0){
         throw new Error(`Expected ${expected}, but got ${result}`)
     }
 })
--- src/operations.ts
+++ src/operations.ts
@@ -1,6 +1,15 @@
 class QuadraticEquation {
-    public solve(a: number, b: number, c: number): number {
-        return 0
+    public solve(a: number, b: number, c: number): number[] {
+        //a*x^2 + b*x + c = 0, a != 0
+
+        const D = b * b - 4 * a * c
+
+        //Корней нет
+        if (D < 0) {
+            return [] as number[]
+        }
+
+        return [0]
     }
 }
 
