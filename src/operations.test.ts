7bce8693d3c9a8ce558fb9f9506f7140e43b81b5
i.egoshin <i.egoshin@prosoftsystems.ru>
2024-04-11 16:47:57 +0500

    preview of steps 9, 10 of the homework

--- src/operations.test.ts
+++ src/operations.test.ts
@@ -3,11 +3,13 @@ const operations = require("./operations")
 
 //a*x^2 + b*x + c = 0, a != 0
 
+const e = 1e-7;//Number.EPSILON
+
 //тест, который проверяет, что для уравнения x^2+1 = 0 корней нет (возвращается пустой массив)
 it('x^2+1 = 0 корней нет', () => {
     const a = 1, b = 0, c = 1
     const expected = 0
-    const result = operations.equation.solve(a, b, c)
+    const result = operations.equation.solve(a, b, c, e)
 
     if(result.length !== 0){
         throw new Error(`Expected ${expected}, but got ${result}`)
@@ -18,7 +20,7 @@ it('x^2+1 = 0 корней нет', () => {
 it('x^2-1 = 0 есть два корня', () => {
     const a = 1, b = 0, c = -1
     const expected = 0
-    const result = operations.equation.solve(a, b, c)
+    const result = operations.equation.solve(a, b, c, e)
 
     if(result[0] != 1 || result[1] != -1){
         throw new Error(`Expected ${expected}, but got ${result}`)
@@ -29,9 +31,24 @@ it('x^2-1 = 0 есть два корня', () => {
 it('x^2+2x+1 = 0 есть один корень', () => {
     const a = 1, b = 2, c = 1
     const expected = 0
-    const result = operations.equation.solve(a, b, c)
+    const result = operations.equation.solve(a, b, c, e)
 
     if(result[0] != -1 || result[1] != -1){
+        throw new Error(`Expected ${expected}, but got ${result}`)
+    }
+})
+
+//коэффициент a не может быть равен 0
+it('коэффициент a не может быть равен 0', () => {
+    const a = 0, b = 2, c = 1
+    const expected = 0
+    let result = [] as number[]
+    try {
+        result = operations.equation.solve(a, b, c, e)
+    } catch (err) {
+        console.log(err);
+    }
+    if(result.length != 0){
         throw new Error(`Expected ${expected}, but got ${result}`)
     }
 })
--- src/operations.ts
+++ src/operations.ts
@@ -1,9 +1,14 @@
 class QuadraticEquation {
-    public solve(a: number, b: number, c: number): number[] {
+    public solve(a: number, b: number, c: number, e: number): number[] {
         //a*x^2 + b*x + c = 0, a != 0
 
         let x1: number = 0
         let x2: number = 0
+
+        if (a > -e && a < e ) {
+            throw new Error('a не может быть равен 0')
+        }
+
         const D = b * b - 4 * a * c
 
         //Корней нет
