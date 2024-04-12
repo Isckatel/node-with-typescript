b9d822e960b6d7da28fafc4b15451e7d0e5cdf31
i.egoshin <i.egoshin@prosoftsystems.ru>
2024-04-12 11:56:06 +0500

    step 9 of homework

--- src/operations.test.ts
+++ src/operations.test.ts
@@ -8,7 +8,7 @@ const e = 1e-7;//Number.EPSILON
 //тест, который проверяет, что для уравнения x^2+1 = 0 корней нет (возвращается пустой массив)
 it('x^2+1 = 0 корней нет', () => {
     const a = 1, b = 0, c = 1
-    const expected = 0
+    const expected = [] as number[]
     const result = operations.equation.solve(a, b, c, e)
 
     if(result.length !== 0){
@@ -19,7 +19,7 @@ it('x^2+1 = 0 корней нет', () => {
 //для уравнения x^2-1 = 0 есть два корня кратности 1 (x1=1, x2=-1)
 it('x^2-1 = 0 есть два корня', () => {
     const a = 1, b = 0, c = -1
-    const expected = 0
+    const expected = [1, -1]
     const result = operations.equation.solve(a, b, c, e)
 
     if(result[0] != 1 || result[1] != -1){
@@ -30,7 +30,7 @@ it('x^2-1 = 0 есть два корня', () => {
 //для уравнения x^2+2x+1 = 0 есть один корень кратности 2 (x1= x2 = -1)
 it('x^2+2x+1 = 0 есть один корень', () => {
     const a = 1, b = 2, c = 1
-    const expected = 0
+    const expected = [-1, -1]
     const result = operations.equation.solve(a, b, c, e)
 
     if(result[0] != -1 || result[1] != -1){
@@ -41,14 +41,7 @@ it('x^2+2x+1 = 0 есть один корень', () => {
 //коэффициент a не может быть равен 0
 it('коэффициент a не может быть равен 0', () => {
     const a = 0, b = 2, c = 1
-    const expected = 0
-    let result = [] as number[]
-    try {
-        result = operations.equation.solve(a, b, c, e)
-    } catch (err) {
-        console.log(err);
-    }
-    if(result.length != 0){
-        throw new Error(`Expected ${expected}, but got ${result}`)
-    }
+    it("is a function", function(){
+        assert.throw(() => { operations.equation.solve(a, b, c, e) }, Error, "Error thrown");
+    });
 })
